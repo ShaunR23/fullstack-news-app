0f084557d72f25a62784266ca80f8d1117a83697
ShaunR23 <shaunradford23@gmail.com>
2022-02-21 15:15:55 -0500

    edited allowed host

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     '127.0.0.1',
     'localhost',
-    'https://fullstack-news-app-sr23.herokuapp.com/'
+    'https://fullstack-news-app-sr23.herokuapp.com/',
 ]
 
 
